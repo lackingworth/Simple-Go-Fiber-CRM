c9a4068fe1470f284614ff013d8d54c39959e7e9
lackingworth <worstface@yandex.ru>
2023-09-28 16:48:32 +0300

    Init

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/lackingworth/Go-Fiber-CRM/database"
+	"github.com/lackingworth/Go-Fiber-CRM/lead"
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	
+	"fmt"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "db_name")
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	fmt.Println("Connection to database successful")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
+}
+
+func main() {
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	app.Listen(3000)
+	defer database.DBConn.Close()
+}
