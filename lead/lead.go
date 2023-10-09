c8e5f9bae5a9f6595f9675593651705c838d3af7
Sergei Rozanov <135345845+lackingworth@users.noreply.github.com>
2023-10-09 11:54:59 +0300

    Update lead.go

--- lead/lead.go
+++ lead/lead.go
@@ -12,7 +12,7 @@ type Lead struct {
 	Name 		string		`json:"name"`
 	Company 	string		`json:"company"`
 	Email 		string		`json:"email"`
-	Phone 		int			`json:"phone"`
+	Phone 		int		`json:"phone"`
 }
 
 func GetLeads(c *fiber.Ctx) {
